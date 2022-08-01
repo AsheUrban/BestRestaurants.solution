712d63b8669db7930a4dad66aaad5a912e3d578c
Ashe Urban <ashe@goldentongue.com>
2022-08-01 12:02:16 -0700

    WIP create restaurant cshtml.
    co-authored-by: Ben Goodrow <bpgoodrow@gmail.com>

--- BestRestaurants/Controllers/RestaurantsController.cs
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -1,32 +1,31 @@
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc;
-// using BestRestaurants.Models;
-// using System.Collections.Generic;
-// using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace BestRestaurants.Controllers
-// {
-//   public class RestaurantsController : Controller
-//   {
-//     private readonly BestRestaurantsContext _db;
+namespace BestRestaurants.Controllers
+{
+  public class RestaurantsController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
 
-//     public RestaurantsController(BestRestaurantsContext db)
-//     {
-//       _db = db;
-//     }
+    public RestaurantsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
 
-//     public ActionResult Index()
-//     {
-//       List<Restaurant> model = _db.Restaurants.Include(restaurant => restaurant.Cuisine).ToList();
-//       return View(model);
-//     }
+    public ActionResult Index()
+    {
+      return View(_db.Restaurants.ToList());
+    }
 
-//     public ActionResult Create()
-//     {
-//       ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "Type");
-//       return View();
-//     }
+    public ActionResult Create()
+    {
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "Type");
+      return View();
+    }
 
 //     [HttpPost]
 //     public ActionResult Create(Restaurant restaurant)
@@ -36,11 +35,14 @@
 //       return RedirectToAction("Index");
 //     }
 
-//     public ActionResult Details(int id)
-//     {
-//       Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(restaurant => restaurant.RestaurantId == id);
-//       return View(thisRestaurant);
-//     }
+      public ActionResult Details(int id)
+      {
+          var thisRestaurant = _db.Restaurants
+              .Include(restaurant => restaurant.JoinEntities)
+              .ThenInclude(join => join.Cuisine)
+              .FirstOrDefault(restaurant => restaurant.RestaurantId == id);
+          return View(thisRestaurant);
+      }
 
 //     public ActionResult Edit(int id)
 //     {
@@ -71,5 +73,5 @@
 //       _db.SaveChanges();
 //       return RedirectToAction("Index");
 //     }
-//   }
-// }
+  }
+}
